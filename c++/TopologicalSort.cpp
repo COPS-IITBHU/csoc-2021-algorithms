6f31f14f59883206e0d2f67484654fa005aaadb9
Eshaan Agarwal <eshaan060202@gmail.com>
2021-06-01 17:46:13 +0530

    Topological Sort Implementation Added

--- c++/TopologicalSort.cpp
+++ c++/TopologicalSort.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 // Implementation of Topological Sorting on a Directed Acyclic Graph (DAG)
 
-// Here we implement linear ordering of vertics of DAG such that for every directed edge a and b, vertex a comes before b in the order using modified DFS.
+// Here we implement linear ordering of vertices of DAG such that for every directed edge a and b, vertex a comes before b in the order using modified DFS.
 
 // Time Complexity - O(V+E)
 // Space Complexity - O(N)
 
 
 // Code Begins 
-vector <long int> adj[100001];//Vector which store the adjacency list
+vector <long int> adj[100001];//Vector which stores the adjacency list
 
 stack <long int> order;//Stack which stores the order
 
@@ -19,18 +19,18 @@ bool vis[100001];// Array storing state of edges (visited/unvisited)
  
 void toposort(long int v)
 {
-    vis[v]=true;// assign the edge visited 
+    vis[v]=true;// assign the edge to visited 
     for(long long int i = 0;i<adj[v].size();i++){
         if(!vis[adj[v][i]])// Every unvisited child of the edge is visited recursively 
         toposort(adj[v][i]);
     }
 
-    order.push(v);//Required edge is pushed in order
+    order.push(v);//Required edge is pushed in order stack
 }
  
 int main(){
 
-	long int n,m;// No of vertics and edges respeectively
+	long int n,m;// No of vertics and edges respectively
 	cin>>n>>m;
 
 	for(long int i = 0;i<m;i++){
