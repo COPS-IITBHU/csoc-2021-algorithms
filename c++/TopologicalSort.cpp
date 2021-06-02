d998eb4f69549667481863e415015a233e5725a1
Eshaan Agarwal <eshaan060202@gmail.com>
2021-06-02 08:16:07 +0530

    Updated TopologicalSort.cpp

--- c++/TopologicalSort.cpp
+++ c++/TopologicalSort.cpp
@@ -1,60 +1,55 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
 // Implementation of Topological Sorting on a Directed Acyclic Graph (DAG)
 
-// Here we implement linear ordering of vertices of DAG such that for every directed edge a and b, vertex a comes before b in the order using modified DFS.
+vector <long int> adj[100001];  // Vector which stores the adjacency list
+stack <long int> order;         // Stack which stores the order
+bool vis[100001];               // Array storing state of edges (visited/unvisited)
 
-// Time Complexity - O(V+E)
-// Space Complexity - O(N)
+void toposort(long int v); 
 
-
-// Code Begins 
-vector <long int> adj[100001];//Vector which stores the adjacency list
-
-stack <long int> order;//Stack which stores the order
-
-bool vis[100001];// Array storing state of edges (visited/unvisited)
- 
-void toposort(long int v)
-{
-    vis[v]=true;// assign the edge to visited 
-    for(long long int i = 0;i<adj[v].size();i++){
-        if(!vis[adj[v][i]])// Every unvisited child of the edge is visited recursively 
-        toposort(adj[v][i]);
-    }
-
-    order.push(v);//Required edge is pushed in order stack
-}
- 
 int main(){
 
-	long int n,m;// No of vertics and edges respectively
+	long int n,m;           // No of vertics and edges respectively
 	cin>>n>>m;
 
 	for(long int i = 0;i<m;i++){
 	    long int u,v;
 	    cin>>u>>v;
-	    adj[u].push_back(v);
-	// vertices containing one of the edges are input in form of adjacency list
+	    adj[u].push_back(v); // vertices containing one of the edges are input in form of adjacency list
+
 	}
 	for(long int i = 1;i<=n;i++){
 
-     //Initializing all the edges to invisited
-	    vis[i]=false;
+	    vis[i]=false;       // Initializing all the edges to unvisited
 	}
 	for(long int i = 1;i<=n;i++){
 	    if(!vis[i]){  
-	    //If the edge is unvisited then we apply toposort
-	        toposort(i);
+
+		toposort(i);    // If the edge is unvisited then we apply toposort
 	    }
 	}
 
- //Order is stored in stack because of stack's LIFO(Last in First Out) nature.
-	while (order.empty() == false){
+
+	while (order.empty() == false){  // Order is shown in output
 			cout << order.top() << " ";
 			order.pop();
 		}
 
 }
+
+void toposort(long int v)
+{
+    vis[v]=true;                // assign edge visited 
+    for(long long int i = 0;i<adj[v].size();i++){
+        if(!vis[adj[v][i]])     // Every unvisited child of the edge is visited recursively 
+        toposort(adj[v][i]);
+    }
+
+    order.push(v);              // Required edge is pushed in order stack
+}
+ 
