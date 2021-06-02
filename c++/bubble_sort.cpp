b38608fc492b091de7afe484aa4e7f76bdd96f7d
iamaks287 <ashwat9149@gmail.com>
2021-06-02 10:16:09 +0530

    modified

--- c++/bubble_sort.cpp
+++ c++/bubble_sort.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int s) 
+void bubbleSort(int arr[], int s);
+
+int main()
 {
-  for (int step = 0; step < (s-1); ++step) 
-  {
-    for (int i = 0; i < s - (step-1); ++i) 
+    int a[] = {-2, 45, 0, 11, -9, 647, 6, -15, 0, 5};
+    int n = sizeof(a) / sizeof(a[0]);
+    bubbleSort(a, n);
+    cout << "Sorted Array in Ascending Order:\n";
+    for (int i = 0; i < n; ++i)
     {
-      if (arr[i] > arr[i + 1]) 
-      {
-        int temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-      }
+        cout << "  " << a[i];
     }
-  }
+    return 0;
 }
 
-void printArray(int arr[], int s) 
+void bubbleSort(int arr[], int s)
 {
-  for (int i = 0; i < s; ++i) 
-  {
-    cout << "  " << arr[i];
-  }
-  cout << "\n";
-}
-
-int main() 
-{
-  int a[] = {-2, 45, 0, 11, -9};
-  int n = sizeof(a) / sizeof(a[0]);
-  bubbleSort(a, n);
-  cout << "Sorted Array in Ascending Order:\n";  
-  printArray(a,n);
+    for (int step = 0; step < (s - 1); ++step)
+    {
+        for (int i = 0; i < s - step - 1; ++i)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
 }
