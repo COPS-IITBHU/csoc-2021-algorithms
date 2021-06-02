d10c0764174d7521a3ffddcbc6da0d87115dcf8a
shubhangi013 <78342516+shubhangi013@users.noreply.github.com>
2021-06-02 11:57:40 +0530

    Implementation of Naive Pattern Searching (#17)
    * added the alorihtm's implementation in c++
    
    * added the algorithm's implementation in c++
    
    * implemented the algorithm and used a separate branch
    
    * implemented the algorithm in a cleaner way
    
    * implemented the algorithm in a cleaner way
    
    * removed the trailing newlines and modifies header files
    
    * removed the executable file

---
+++ c++/naive_pattern_searching.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include<strings.h>
+#include<iostream>
+using namespace std;
+
+vector <int> search_stringSearch(string search_string,string string);
+
+
+int main()
+{
+	string s;
+    string search_s;
+	vector <int> index;
+	cout<<"Enter the string here : ";
+	getline(cin,s);
+	cout<<"Enter the pattern you want to search here : ";
+	getline(cin,search_s);
+	search_stringSearch(search_s, s);
+	index=search_stringSearch(search_s, s);
+	cout << "The entered pattern occurs in the entered string at:   "<<endl;
+	for(int i=0;i<index.size();i++)
+	cout<<index[i]<<" ";
+	return 0;
+}
+
+vector <int> search_stringSearch(string search_string,string string)
+{
+	int m = search_string.size();
+	int n = string.size();
+	vector <int> a;
+
+	for (int i = 0; i <= n - m; i++) {
+		int j;
+
+		for (j = 0; j < m; j++)
+			if (string[i + j] != search_string[j])
+				break;
+        if (j == m)
+			a.push_back(i);
+	}
+	return a;
+}
