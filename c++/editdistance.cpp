769f9d4e525e7242bebd9a89a08a176ebd53581e
Devanshu Agrawal <dev12345spark@gmail.com>
2021-06-04 19:21:25 +0530

    mycommit

---
+++ c++/editdistance.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int min(int x, int y, int z){ 
+    
+    return min(min(x, y), z); 
+}
+ 
+int editDistanceDP(string str1, string str2, int m, int n)
+{
+
+    int dp[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            
+            if (i == 0) dp[i][j] = j; 
+            else if (j == 0) dp[i][j] = i;
+            else if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+            else dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]);
+        }
+    }
+ 
+    return dp[m][n];
+}
+ 
+int main()
+{
+    string str1;
+    cin>>str1;
+
+    string str2;
+    cin>>str2;
+ 
+    cout << "Minimum numbers of edits required to convert" << str1 << "to" << str2 << editDistanceDP(str1, str2, str1.length(),str2.length());
+ 
+    return 0;
+}
